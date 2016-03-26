e62c3e4a515ac2a5860a00632657a77cbdf69779
shahas <shahas@bullfin.ch>
2016-03-26 21:02:31 +0530

    create show methods added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < InheritedResources::Base
 
 	respond_to :html, :xml, :json
+	skip_before_filter  :verify_authenticity_token
 
 	def index
 		@users = User.all
-		render json: @users
+		render json: {users: @users} , status: 200
 	end
+
+	def show
+		@user = User.find(params[:id])
+		render json: @user
+	end
+
+	def user_params
+	   params.require(:user).permit(:name, :email, :encrypted_password)
+  	end
 
 
 end
